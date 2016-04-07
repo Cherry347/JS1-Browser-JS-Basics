330985a57377da7defcdca366ae8ce4cf0f00a76
cherry347 <StillLoriLeigh@gmail.com>
2016-04-07 22:58:27 +0000

    Function Practice #1

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
+// tripleFive
+var tripleFive= function() {
+    for(var i=0; i< 3; i ++) {
+        console.log("Five!");
+    	
+    }
+}
+tripleFive()
+
+// lastLetter
 var lastLetter= function(n) {
     return n[n.length -1];
 }
 lastLetter("cherry")
+
+// square
+var square= function(n) {
+    return n*n;
+}
+square()
+
+//negate
+var negate= function(n) {
+	if(n < 0) {
+		return Math.abs(n);
+	}
+	else {
+		return n - n*2;
+	}
+}
+negate()
+
+//toArray
+var myArray= [];
+var toArray= function(x,y,z) {
+	return myArray.push(x,y,z);
+}
+toArray(1,2,3)
+console.log(myArray)
+
